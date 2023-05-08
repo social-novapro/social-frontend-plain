4b6f0f6f70d9bb40f12d0330998bf417e658d1ea
Daniel Kravec <daniel@kravec.com>
2023-05-08 00:19:44 -0400

    alerts updates you can fetch specfic alerts you can create an alert updated alertEle in case no title

--- html/alerts/script.js
+++ html/alerts/script.js
@@ -7,9 +7,11 @@ var currentUserLogin = { }
 var LOCAL_STORAGE_LOGIN_USER_TOKEN ='social.loginUserToken'
 var baseURL
 var headers = {
+    'Content-Type': 'application/json',
     "devtoken" : "6292d8ae-8c33-4d46-a617-4ac048bd6f11",
     "apptoken" : "3610b8af-81c9-4fa2-80dc-2e2d0fd77421"
 }
+
 var verifiedConnection = false
 
 var apiURL = `${config ? `${config.current == "prod" ? config.prod.api_url : config.dev.api_url}` : 'https://interact-api.novapro.net/v1' }`
@@ -31,20 +33,45 @@ async function checkLogin() {
     else return setupUI()
 }
 
-
 function setupUI() {
     document.getElementById('mainFeed').innerHTML = `
         <div id="latestAlert"></div>
-        <div id="verifyRequests">
+        <div class="publicPost" id="create">
             <a onclick="setupCreate()" class="buttonStyled">Create Alert</a>
         </div>
-        <div id="listVerifications">
+        <div class="publicPost" id="list">
             <a onclick="listAlerts()" class="buttonStyled">List Alerts</a>
         </div>
+        <div id="getAlert">
+            <div class="publicPost">
+                <h1>Get Alert</h1>
+                <form id="requestAlert" onsubmit="userRequestedAlert()">
+                    <div class="userInfo">
+                    <p>Enter Alert ID:</p>
+                    <input class="contentMessage userEditForm" id="alertIDPlace" placeholder="Alert ID" type="text"></input>
+                    <div class="signInDiv">
+                        <button class="buttonStyled" type="submit">Fetch</button>
+                    </div>
+                </form>
+            </div>     
+        </div>
         <div id="alertCreate"></div>
+        <div id="newAlert"></div>
         <div id="alertList"></div>
+        <div id="alert"></div>
     `
+
+    document.getElementById("requestAlert").addEventListener("submit", function (e) { e.preventDefault()});
     getLatestAlert();
+}
+
+async function userRequestedAlert() {
+
+    const alertID = document.getElementById('alertIDPlace').value
+    const data = await fetchAlert(alertID);
+    // if ()
+    console.log(data);
+    document.getElementById('alert').innerHTML = alertEle(data.alert);
 }
 
 async function fetchAlert(alertID) {
@@ -56,7 +83,7 @@ async function fetchAlert(alertID) {
     const alertFetch = await response.json();
     console.log(alertFetch);
 
-    if (!response.ok) return console.log(response);
+    if (!response.ok) return { error: true, };
     else return alertFetch;
 }
 
@@ -93,7 +120,71 @@ async function listAlerts() {
     document.getElementById('alertList').innerHTML = ele;
 }
 
+async function sendCreateAlert() {
+    var title = document.getElementById('createAlertForm').value;
+    var content = document.getElementById('alertContentCreate').value;
+    var linked = document.getElementById('alertLinkedCreate').value;
+
+    const data = {
+        title: title || null,
+        content: content,
+        postID: linked || null
+    }
+    console.log(data)
+
+    const response = await fetch(`${apiURL}/alert/create/`, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(data)
+    })
+
+    // if (response.status != 200) 
+    console.log(response)
+    const alertData = await response.json()
+    console.log(alertData)
+
+    if (response.ok) {
+        document.getElementById('newAlert').innerHTML = `
+            <div class="publicPost">
+                <h1>Alert Created!</h1>
+                ${alertEle(alertData.alert)}
+            </div>
+        `
+    }
+    else return showModal(`<p>Error: ${alertData.code}\n${alertData.msg}</p>`)
+
+    if (userData.login === true) return redirection()
+}
+
 function setupCreate() {
+    // title
+    // content
+    // linked post
+    document.getElementById('alertCreate').innerHTML = `
+        <div class="userInfo">
+        <h1>Create Alert</h1>
+            <form onsubmit="sendCreateAlert()" id="createAlertForm">
+                <div class="userInfo">
+                    <p>Enter Title:</p>
+                    <p><input class="contentMessage userEditForm" id="alertTitleCreate" placeholder="Title" type="text" "></p>
+                </div>
+                <div class="userInfo">
+                    <p>Enter Content:*</p>
+                    <p><input class="contentMessage userEditForm" id="alertContentCreate" placeholder="Password" type="text"></p>
+                </div>
+                <div class="userInfo">
+                    <p>Enter Linked Post:</p>
+                    <p><input class="contentMessage userEditForm" id="alertLinkedCreate" placeholder="Password" type="text"></p>
+                </div>
+                <div class="signInDiv">
+                    <button class="buttonStyled" type="submit">Create</div>
+                </div>
+            </form>
+        </div>
+        <div class="search">
+    `
+
+    document.getElementById("createAlertForm").addEventListener("submit", function (e) { e.preventDefault()})
 
 }
 
@@ -102,7 +193,11 @@ function alertEle(alert) {
     const { alertID, title, content } = alert;
     return `
         <div class="publicPost" id="alertID">
-            <h1>${title}</h3>
+            ${title ? 
+                `
+                    <h1>${title}</h1>
+                ` : ``
+            }
             <p>${content}</p>
         </div>
     `;
